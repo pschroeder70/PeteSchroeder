ed99ddf2a2680663c06f3e2f95b84b8c052aa5eb
Pete Schroeder <pete.schroeder@protonmail.com>
2021-11-29 13:32:50 -0500

    push
    let's get it started...ha...

---
+++ src/Start.js
@@ -0,0 +1,16 @@
+import './Start.css';
+
+function Start() {
+  return (
+    <div>
+      <div className="start-header">
+        <div>
+          <h1>Pete Schroeder</h1>
+        </div>
+        <a href="https://www.linkedin.com/in/peteschroeder/">LinkedIn</a>
+      </div>
+    </div>
+  );
+}
+
+export default Start;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Start from './Start';
+
+ReactDOM.render(
+  <>
+    <Start />
+  </>,
+  document.getElementById('root')
+);
+
