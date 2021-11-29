8a9fa0b3caffa9fc5f88326413460afd25dc6fd0
Pete Schroeder <pete.schroeder@protonmail.com>
2021-11-29 15:12:55 -0500

    playing

--- src/Start.js
+++ src/Start.js
@@ -4,10 +4,13 @@ function Start() {
   return (
     <div>
       <div className="start-header">
-        <div>
-          <h1>Pete Schroeder</h1>
-        </div>
-        <p>More info at <a href="https://www.linkedin.com/in/peteschroeder/">LinkedIn</a></p>
+        <p className="start-header__name">Pete Schroeder</p>
+        <ul className="start-header__list">
+          <li className="start-header__list-item"><a href="https://www.linkedin.com/in/peteschroeder/">LinkedIn</a> </li>
+          <li className="start-header__list-item"><a href="https://github.com/pschroeder70">GitHub</a> </li>
+          <li className="start-header__list-item"><a href="https://codepen.io/PeteS">CodePen</a> </li>
+          <li className="start-header__list-item"><a href="https://letterboxd.com/4kFan/">Letterboxd</a></li>
+        </ul>
       </div>
     </div>
   );
